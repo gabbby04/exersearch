be663c64a700de9f1232304d8940fcf7004ce88c
Ahron Javier <ahronjavier16@gmail.com>
2026-03-11 08:33:02 +0800

    css

--- frontend/src/pages/user/WhyExersearch.jsx
+++ frontend/src/pages/user/WhyExersearch.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "./WhyExersearch.css";
 
+import ScrollThemeWidget from "../../utils/ScrollThemeWidget";
 import {
   Zap,
   Map,
